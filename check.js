d9e37fde5c84b9d1e31e353a895ff49151462eba
华 <sliphua@outlook.com>
2022-05-23 17:45:45 +0800

    print all sources & refactor

--- check.js
+++ check.js
@@ -3,25 +3,26 @@ const path = require('path');
 
 const coverageDir = './coverage';
 
-let sourceCount = 0;
-const malformedSourceList = [];
+const sources = [];
+const malformedSources = [];
 
 fs.readdirSync(coverageDir).forEach((fileName) => {
   const coverage = JSON.parse(fs.readFileSync(path.resolve(coverageDir, fileName), 'utf8'));
   const sourceMapCache = coverage['source-map-cache'];
   if (!sourceMapCache) return;
-  const sources = Object.values(sourceMapCache).flatMap((payload) => payload.data?.sources || []);
-  sourceCount += sources.length;
-  sources.forEach((source) => {
-    if (/^file:\/\/\/[A-Z]/.test(source)) return;
-    malformedSourceList.push(source);
-  });
+  Object.values(sourceMapCache)
+    .flatMap((cache) => cache.data?.sources || [])
+    .forEach((source) => {
+      sources.push(source);
+      if (/^file:\/\/\/[A-Z]:/.test(source)) return;
+      malformedSources.push(source);
+    });
 });
 
-console.log(`Found ${sourceCount} sources in total.`);
+console.log(`Found ${sources.length} sources in total:`, sources);
 
-const malformedLength = malformedSourceList.length;
+const malformedLength = malformedSources.length;
 if (malformedLength > 0) {
-  console.log(`${malformedLength} malformed:`, malformedSourceList);
+  console.log(`${malformedLength} malformed:`, malformedSources);
   process.exitCode = 1;
 }
